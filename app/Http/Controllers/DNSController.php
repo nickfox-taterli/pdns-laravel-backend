0184660aba545a73f0f91e26c58e7a47ea19b96e
root <root@WIKIHOST-21091815GQVI>
2021-10-27 23:38:48 +0800

    添加NS获取的实现

--- app/Http/Controllers/DNSController.php
+++ app/Http/Controllers/DNSController.php
@@ -11,6 +11,9 @@ class DNSController extends Controller
     private $qtype = ['A', 'A6', 'AAAA', 'AFSDB', 'ALIAS', 'CAA', 'CDNSKEY', 'CDS', 'CERT', 'CNAME', 'DHCID', 'DLV', 'DNSKEY', 'DNAME', 'DS', 'EUI48', 'EUI64', 'HINFO', 'IPSECKEY', 'KEY', 'KX', 'LUA', 'LOC', 'MAILA', 'MAILB', 'MINFO', 'MR', 'MX', 'NAPTR', 'NS', 'NSEC', 'NSEC3', 'NSEC3PARAM', 'OPENPGPKEY', 'OPT', 'RKEY', 'RP', 'RRSIG', 'SIG', 'SPF', 'SRV', 'SSHFP', 'TLSA', 'TKEY', 'TSIG', 'TXT', 'WKS', 'URI'];
     // RNDS域名
     private $suffix = 'taterli.com';
+    // DNS服务器域名
+    private $ns1 = 'ns1.taterli.com';
+    private $ns2 = 'ns2.taterli.com';
 
     public function lookup(Request $request)
     {
@@ -90,6 +93,46 @@ class DNSController extends Controller
             }
         }
 
+        // NS提取模式
+        if (substr($request->qname, strpos($request->qname, '.in-addr.arpa.')) === '.in-addr.arpa.') {
+            $dot = count(explode('.', $request->qname));
+            if ($dot >= 4 && $dot <= 6) {
+                $records = DB::table('records')->where('type', 'NS')->where('name', trim($request->qname, '.'))->get();
+                foreach ($records as $record) {
+                    array_push($ret, ["qtype" => $qtype, "qname" => $request->qname, "content" => $record->content, $record->ttl]);
+                }
+                if ($records->count() == 0) {
+                    array_push($ret, ["qtype" => 'NS', "qname" => trim($request->qname, '.'), "content" => $this->ns1, 3600]);
+                    array_push($ret, ["qtype" => 'NS', "qname" => trim($request->qname, '.'), "content" => $this->ns2, 3600]);
+                }
+            }
+        }else if (substr($request->qname, strpos($request->qname, '.ip6.arpa.')) === '.ip6.arpa.') {
+            $dot = count(explode('.', $request->qname));
+            if ($dot >= 11 && $dot <= 15) {
+                $records = DB::table('records')->where('type', 'NS')->where('name', trim($request->qname, '.'))->get();
+                foreach ($records as $record) {
+                    array_push($ret, ["qtype" => $qtype, "qname" => $request->qname, "content" => $record->content, $record->ttl]);
+                }
+                if ($records->count() == 0) {
+                    array_push($ret, ["qtype" => 'NS', "qname" => trim($request->qname, '.'), "content" => $this->ns1, 3600]);
+                    array_push($ret, ["qtype" => 'NS', "qname" => trim($request->qname, '.'), "content" => $this->ns2, 3600]);
+                }
+            }
+        }else{
+            // 普通域名
+            $domain = trim($request->qname, '.');
+            $domain = explode('.', $domain);
+            $domain = $domain[count($domain) - 2] . '.' . $domain[count($domain) - 1];
+            $records = DB::table('records')->where('type', 'NS')->where('name', trim($request->qname, '.'))->get();
+            foreach ($records as $record) {
+                array_push($ret, ["qtype" => $qtype, "qname" => $request->qname, "content" => $record->content, $record->ttl]);
+            }
+            if ($records->count() == 0) {
+                array_push($ret, ["qtype" => 'NS', "qname" => trim($request->qname, '.'), "content" => $this->ns1, 3600]);
+                array_push($ret, ["qtype" => 'NS', "qname" => trim($request->qname, '.'), "content" => $this->ns2, 3600]);
+            }
+        }
+
         // 其他记录
         foreach ($this->qtype as $qtype) {
             // 这两个涉及RDNS情况刚才已经处理过了,因此不重复处理.
@@ -114,16 +157,13 @@ class DNSController extends Controller
             }
 
             // 泛解释逻辑,检索出某域名结尾全部*内容.
-            $domain = trim($request->qname, '.');
-            $domain = explode('.', $domain);
-            $domain = $domain[count($domain) - 2] . '.' . $domain[count($domain) - 1];
             $records = DB::table('records')->where('type', $qtype)->where('name', 'like', '%*%.' . $domain)->get();
             // 检查当前域名是否出于这个*内.
             foreach ($records as $record) {
                 $suffix = ltrim($record->name, '*') . '.';
                 if (substr($request->qname, strpos($request->qname, $suffix)) === $suffix) {
                     // 进入这里就是匹配到泛解释
-                    $records = DB::table('records')->where('type', $qtype)->where('name', '*.'.trim($suffix, '.'))->get();
+                    $records = DB::table('records')->where('type', $qtype)->where('name', '*.' . trim($suffix, '.'))->get();
                     foreach ($records as $record) {
                         if (strcmp($qtype, 'MX') == 0) {
                             array_push($ret, ["qtype" => $qtype, "qname" => $request->qname, "content" => $record->prio . ' ' . $record->content, $record->ttl]);
@@ -149,7 +189,7 @@ class DNSController extends Controller
 
         return response()->json(["result" => $ret]);
     }
-    
+
     public function getAllDomainMetadata(Request $request)
     {
         return response()->json(["result" => ["PRESIGNED" => ["0"]]]);
